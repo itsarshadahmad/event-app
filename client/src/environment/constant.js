491f6bf82ea7f046ba79a76e04221799e12966fa
Arshad Ahmad <arshadxdev@gmail.com>
2024-12-11 18:25:38 +0530

    fix: proxy issues fixed

--- client/src/environment/constant.js
+++ client/src/environment/constant.js
@@ -1,2 +1,3 @@
 // export const API_URL = import.meta.env.API_URL;
-export const API_URL = "http://localhost:8000/api/v1";
+// export const API_URL = "http://localhost:8000/api/v1";
+export const API_URL = import.meta.env.VITE_API_URL;
